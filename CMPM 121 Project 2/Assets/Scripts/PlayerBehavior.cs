8a5f70bfc5339f92f49a38c813f770823f5750ff
tnoell <tnoell@ucsc.edu>
2021-08-03 01:56:43 -0700

    added player control

--- CMPM 121 Project 2/Assets/Scripts/PlayerBehavior.cs
+++ CMPM 121 Project 2/Assets/Scripts/PlayerBehavior.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class PlayerBehavior : MonoBehaviour
 {
     public float _moveSpeed = 1;
+    public float _rotationSpeed = 1;
     private CharacterController _characterController;
 
     // Start is called before the first frame update
     void Start()
     {
+        //https://docs.unity3d.com/ScriptReference/Cursor-lockState.html
+        Cursor.lockState = CursorLockMode.Confined;
+
         _characterController = GetComponent<CharacterController>();   
     }
 
     // Update is called once per frame
     void Update()
     {
-        _characterController.SimpleMove(new Vector3(Input.GetAxis("Horizontal") * _moveSpeed * Time.deltaTime, 0, Input.GetAxis("Vertical") * _moveSpeed * Time.deltaTime));
+        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X") * _rotationSpeed * Time.deltaTime, 0));
+        Vector3 newPosition = transform.forward * Input.GetAxis("Vertical") * _moveSpeed * Time.deltaTime + transform.right * Input.GetAxis("Horizontal") * _moveSpeed * Time.deltaTime;
+        _characterController.SimpleMove(newPosition);
     }
 }
