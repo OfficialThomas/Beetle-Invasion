e3b6703497a1d13c2bf539f9394430012de85ee4
tnoell <tnoell@ucsc.edu>
2021-08-08 15:52:00 -0700

    finalize in-game content

--- CMPM 121 Project 2/Assets/Scripts/CameraBehavior.cs
+++ CMPM 121 Project 2/Assets/Scripts/CameraBehavior.cs
@@ -5,39 +5,23 @@ using UnityEngine;
 public class CameraBehavior : MonoBehaviour
 {
     public GameObject _player;
-    private bool _fps = false;
-    public Vector3 _3rdOffset;
-    public Vector3 _1stOffset;
 
     // Start is called before the first frame update
     void Start()
-    {
-        transform.position = _3rdOffset; 
+    { 
+
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            if (_fps)
-            {
-                transform.position = _3rdOffset;
-                _fps = false;
-            }
-            else
-            {
-                transform.position = _1stOffset;
-                _fps = true;
-            }
-        }   
+    { 
 
     }
 
     private void LateUpdate()
     {
         transform.rotation = _player.transform.rotation;
-        if (!_fps)
+        if (this.CompareTag("3rdPerson"))
         {
             transform.LookAt(_player.transform.position);
         }
